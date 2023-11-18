5e65016dfd060d226e54ade6dd765c9edd803f9f
Zhiyi_Yang_YorkU <niko123@my.yorku.ca>
2023-11-18 04:27:16 -0500

    按下P出现暂停菜单（游戏不暂停）

--- OutsideSkyline_Alpha/Script/Level.gd
+++ OutsideSkyline_Alpha/Script/Level.gd
@@ -1,31 +1,42 @@
 extends Node3D
 
 
+var playerNode: Node3D
+var player: PackedScene 	= preload("res://OutsideSkyline_Alpha/Scene/Character/Player.tscn")
+
+var pauseUINode: Control
+var pauseUI: PackedScene 	= preload("res://OutsideSkyline_Alpha/Scene/UI/UI.tscn")
+
 const PLAYER_SPAWN_POSITION: Vector3 	= Vector3.ZERO
-var player: PackedScene 				= preload("res://OutsideSkyline_Alpha/Scene/Character/Player.tscn")
 
 
 
 func _ready() -> void:
-	spawnScene(player, self, PLAYER_SPAWN_POSITION)
-
+	playerNode = spawnScene(player, self, PLAYER_SPAWN_POSITION)
+	pauseUINode = instantiateScene(pauseUI, self).get_node("GamePauseMenu")
+	pauseUINode.visible = false
 
 func _process(delta: float) -> void:
-	pass
+	pauseGame()
+	
+
+func pauseGame() -> void:
+	if Input.is_action_just_pressed("ui_pause") and playerNode.is_on_floor():
+		pauseUINode.visible = not pauseUINode.visible
+		print("bob")
 
 
-
-func instantiateScene(targetScene: PackedScene, parent: Node) -> Node:
-	var target: Node = targetScene.instantiate()
+func instantiateScene(targetScene: PackedScene, parent: Node3D) -> Node3D:
+	var target: Node3D = targetScene.instantiate()
 	parent.add_child(target)
 	return target
 
-func instantiatePath(targetPath: String, parent: Node) -> Node:
-	var target: Node = load(targetPath).instantiate()
+func instantiatePath(targetPath: String, parent: Node3D) -> Node3D:
+	var target: Node3D = load(targetPath).instantiate()
 	parent.add_child(target)
 	return target
 
-func spawnScene(targetScene: PackedScene, parent: Node, spawnPos: Vector3) -> Node:
-	var target: Node = instantiateScene(targetScene, parent)
+func spawnScene(targetScene: PackedScene, parent: Node3D, spawnPos: Vector3) -> Node3D:
+	var target: Node3D = instantiateScene(targetScene, parent)
 	target.global_position = spawnPos
 	return target
