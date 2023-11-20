fb2b12387fd0ef6f76d6b2d30ddffa8d82b2ec69
Zhiyi_Yang_YorkU <niko123@my.yorku.ca>
2023-11-20 13:57:23 -0500

    UI逻辑修改，可运行

--- OutsideSkyline_Alpha/Script/GameplayUI/GameManager.gd
+++ OutsideSkyline_Alpha/Script/GameplayUI/GameManager.gd
@@ -4,39 +4,57 @@ extends Node3D
 @onready var gameUIManager: GameplayUI_Manager = $"../UI"
 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pauseGame()
 
+
 func pauseGame() -> void:
 	if Input.is_action_just_pressed("ui_pause"):
-		if gameUIManager.isOptionToggled:
-			gameUIManager.togglePauseUI(true)
-			gameUIManager.isOptionToggled = not gameUIManager.isOptionToggled
-			gameUIManager.toggleOptionUI(gameUIManager.isOptionToggled)
-		else:
-			gameUIManager.isPauseToggled = not gameUIManager.isPauseToggled
-			gameUIManager.togglePauseUI(gameUIManager.isPauseToggled)
-		
+		# _toggleHow2playUIHelper()
+		_toggleOptionUIHelper()
 
 
-#signal process
+#Helpers
+func _toggleHow2playUIHelper() -> void:
+	if gameUIManager.isOptionToggled:
+		gameUIManager.togglePauseUI(true)
+		gameUIManager.isHow2PlayToggled = not gameUIManager.isHow2PlayToggled
+		gameUIManager.toggleHow2playUI(gameUIManager.isHow2PlayToggled)
+	else:
+		gameUIManager.isPauseToggled = not gameUIManager.isPauseToggled
+		gameUIManager.togglePauseUI(gameUIManager.isPauseToggled)
+
+
+func _toggleOptionUIHelper() -> void:
+	if gameUIManager.isOptionToggled:
+		gameUIManager.togglePauseUI(true)
+		gameUIManager.isOptionToggled = not gameUIManager.isOptionToggled
+		gameUIManager.toggleOptionUI(gameUIManager.isOptionToggled)
+	else:
+		gameUIManager.isPauseToggled = not gameUIManager.isPauseToggled
+		gameUIManager.togglePauseUI(gameUIManager.isPauseToggled)
+
+
+#按下按钮效果
 func _on_game_continue_button_button_up() -> void:
 	gameUIManager.isPauseToggled = false
 	gameUIManager.togglePauseUI(false)
 	gameUIManager.toggleOptionUI(false)
+	gameUIManager.toggleHow2playUI(false)
+
+
+func _on_game_how_2_play_button_button_up() -> void:
+	print("How to Play Pressed")
+	# gameUIManager.isHow2PlayToggled = not gameUIManager.isHow2PlayToggled
+	# gameUIManager.toggleHow2playUI(gameUIManager.isHow2PlayToggled)
+	# gameUIManager.gamePauseMenu.visible = false
 
 
 func _on_game_option_button_button_up() -> void:
-#	print(gameUIManager.isOptionToggled)
-#	print("option button pressed")
 	gameUIManager.isOptionToggled = not gameUIManager.isOptionToggled
 	gameUIManager.toggleOptionUI(gameUIManager.isOptionToggled)
 	gameUIManager.gamePauseMenu.visible = false
 
+
 func _on_game_pause_button_button_up() -> void:
 	SystemHQ.quitGame()
-
-
-
--- OutsideSkyline_Alpha/Script/GameplayUI/GameplayUI_Manager.gd
+++ OutsideSkyline_Alpha/Script/GameplayUI/GameplayUI_Manager.gd
@@ -1,51 +1,42 @@
 class_name GameplayUI_Manager
 extends Node3D
 
-@onready var blackBackground: ColorRect 	= $Control_Game/BlackBackground
-@onready var gamePauseMenu: BoxContainer 	= $Control_Game/GamePause
-@onready var gameOptionMenu: BoxContainer 	= $Control_Game/GameOption
+@onready var blackBackground: ColorRect = $Control_Game/BlackBackground
+@onready var gamePauseMenu: BoxContainer = $Control_Game/GamePause
+@onready var gameOptionMenu: BoxContainer = $Control_Game/GameOption
+@onready var gameHow2playMenu: BoxContainer = $Control_Game/GameHow2play
 
 var isPauseToggled: bool = false
+var isHow2PlayToggled: bool = false
 var isOptionToggled: bool = false
 
-# TODO: 实现暂停
+
 func _ready() -> void:
 	togglePauseUI(false)
+	toggleHow2playUI(false)
 	toggleOptionUI(false)
 
 
-func _process(delta: float) -> void:
-	pass
-
-
-
-#helper
+#helpers
 func togglePauseUI(toggle: bool) -> void:
+	##不起效
 	if toggle:
 		blackBackground.visible = true
 		gamePauseMenu.visible = true
-		#TODO: 给我停下来啊！！！
+		# TODO: 给我停下来啊！！！
 		get_tree().paused = true
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	else:
 		blackBackground.visible = false
 		gamePauseMenu.visible = false
-		#TODO: 给我停下来啊！！！
+		# TODO: 给我停下来啊！！！
 		get_tree().paused = false
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+
+func toggleHow2playUI(toggle: bool) -> void:
+	gameHow2playMenu.visible = toggle
+
+
 func toggleOptionUI(toggle: bool) -> void:
-	if toggle:
-#		blackBackground.visible = true
-		gameOptionMenu.visible = true
-#		gamePauseMenu.visible = false
-#		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	else:
-#		blackBackground.visible = false
-		gameOptionMenu.visible = false
-#		gamePauseMenu.visible = true
-#		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-
-
-
+	gameOptionMenu.visible = toggle
